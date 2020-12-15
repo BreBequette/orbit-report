c0e0e27a5601f6eb9d59e18d13160033c02cb6a9
Bre Bequette Reuther <67883123+BreBequette@users.noreply.github.com>
2020-12-14 18:12:48 -0600

    finished parts 1-3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SatelliteComponent } from './satellite/satellite.component';
+import { OrbitListComponent } from './orbit-list/orbit-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SatelliteComponent,
+    OrbitListComponent
   ],
   imports: [
     BrowserModule
